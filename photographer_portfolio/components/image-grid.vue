b1c160cbc11d6a28fe0178d3201535aaf1409de6
Calin <cotuna.calin@yahoo.com>
2024-11-14 13:33:07 +0200

    Test - firebase

--- photographer_portfolio/components/image-grid.vue
+++ photographer_portfolio/components/image-grid.vue
@@ -9,7 +9,6 @@ const props = defineProps({
 })
 
 const photoUrls = ref([])
-const isLoading = ref(true)
 
 onMounted(async () => {
     try {
@@ -21,36 +20,20 @@ onMounted(async () => {
             const downloadUrl = await getDownloadURL(item)
             photoUrls.value.push(downloadUrl)
         }
-        isLoading.value = false
     } catch (error) {
         console.error('Error retrieving photos:', error)
-        isLoading.value = false
     }
 })
 </script>
 
 <template>
-    <div v-if="isLoading" class="flex justify-center items-center mt-10">
-        <div class="border-4 border-emerald-300 border-t-emerald-500 rounded-full w-12 h-12 animate-spin"></div>
-    </div>
-    <div v-else class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         <div v-for="photoUrl in photoUrls" :key="photoUrl">
             <img :src="photoUrl" class="w-full h-auto" alt="Photo" />
         </div>
     </div>
 </template>
+
 <style scoped>
-@keyframes spin {
-    from {
-        transform: rotate(0deg);
-    }
-
-    to {
-        transform: rotate(360deg);
-    }
-}
-
-.animate-spin {
-    animation: spin 1s linear infinite;
-}
+/* Any styles for your component */
 </style>
