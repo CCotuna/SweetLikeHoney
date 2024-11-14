454e80d885e125ce2e1bddbc9b43f0683a8796b8
Calin <cotuna.calin@yahoo.com>
2024-11-14 13:20:25 +0200

    Test - Changed pic

--- photographer_portfolio/pages/index.vue
+++ photographer_portfolio/pages/index.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="flex flex-col items-center">
+    <image-grid folder-path="images" />
         <Herosection />
-        <div
+        <!-- <div
             class="mt-10 mb-20 p-5 md:p-0 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 items-start ">
             <Servicecard />
             <Servicecard />
             <Servicecard />
-        </div>
-        <image-grid folder-path="images" />
+        </div> -->
     </div>
 </template>
 <script setup></script>
-<style scoped></style>
+<style scoped></style>  
